2c3bfb6c48c5f19ed4fe029b4f134adf034c347e
chuk-chuk <yulia.litvinchuk@alpima.com>
2022-10-13 15:41:41 +0100

    initial set up of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full md:w-3/4 lg:w-1/2">
+      <h1 className="text-2xl underline text-red-600">Feedback Page</h1>
     </div>
   );
 }
